803ecf00250aec657d8246e3acade54e2c2ef88d
Foodieballball <125648391+Foodieballball@users.noreply.github.com>
2025-04-13 03:28:44 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
-```typescript
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-```
